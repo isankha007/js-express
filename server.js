b03d7b422890ab710bbed4c5ed2c2bbd1aced0c5
Sankhadeep Chatterjee <sankhadeep2007@gmail.com>
2023-09-10 18:33:48 +0530

    minor changes

--- server.js
+++ server.js
@@ -1,20 +1,27 @@
 const express = require('express')
 const app =express()
 
-app.set('view engine','ejs')
+app.use(express.static("public"))
 
-app.get("/",(req,res)=>{
-    console.log(' here ')
-    //res.status(500).json({message:"hi"});
-    res.render('index',{text:'world'});
-})
+app.set('view engine','ejs')
+app.use(logger)
+
+// app.get("/",(req,res)=>{
+//     console.log(' here ')
+//     //res.status(500).json({message:"hi"});
+//     res.render('index',{text:'world'});
+// })
 
 const userRouter = require('./routes/users');
-const postRouter = require('./routes/users');
+// const postRouter = require('./routes/users');
+function logger(req,res,next){
+    console.log(req.originalUrl);
+    next();
+}
 
 
 app.use('/users',userRouter)
-app.use('/users',postRouter)
+// app.use('/users',postRouter)
 
 
 app.listen(3000) 
